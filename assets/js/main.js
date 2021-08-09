95da2ac4fbbed0f23e0e58c6e870251f02e20e66
Utasi Bence <utasibence@gmail.com>
2021-08-09 20:12:46 +0200

    hamarosan oldal hozzáadása

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,8 +177,8 @@
     typed_strings = typed_strings.split(",");
     new Typed(".typed", {
       strings: typed_strings,
-      loop: true,
-      typeSpeed: 50,
+      loop: false,
+      typeSpeed: 200,
       backSpeed: 50,
       backDelay: 2000,
     });
